f4883aba68fae60c06be2f7dd024cd11d19660a5
massndp <bayu8989.88@gmail.com>
2021-09-23 12:12:39 +0700

    Export & Import Componen

--- App.js
+++ App.js
@@ -1,111 +1,26 @@
+/* eslint-disable no-unused-vars */
 import React, { Component } from 'react';
-import { Image, StyleSheet, Text, TextInput, View} from 'react-native';
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
 import mackbook from './macbook.jpg';
+import SampleComponent from './SampleComponent';
+import StylingReactNativeComponent from './StylingReactNativeComponent';
 
 const App = () => {
-  return <StylingReactNativeComponent />;
-      
-};
-
-
-const SampleComponent = () => {
   return (
     <View>
-      <View style={{width: 100, height: 100, backgroundColor: '#00b894'}}/>
-      <Text>Hello</Text>
-      <Text>Bayu</Text>
-      <Text>Adi</Text>
-      <Home />
-      <Photo />
-      <TextInput style={{borderWidth: 1}} />
-      <BoxGreen />
-      <Profile />
-  </View>
-  );
-};
-
-
-const StylingReactNativeComponent = () => {
-  return(
-    <View>
-      <Text style={styles.text}>Styling Component</Text>
-      <View style={{
-          width: 100, 
-          height: 100,
-          backgroundColor: '#00b894', 
-          borderWidth: 2,
-          marginTop: 20,
-          marginLeft: 20
-         }} 
-      />
-
-      <View style={{padding: 12, backgroundColor: '#f2f2f2', width: 212}}>
-          <Image source={mackbook} style={{width: 188 , height: 107, borderRadius: 8}} />
-          <Text style={{fontSize: 14, fontWeight:'bold', marginTop: 16}}>
-           New Macbook Pro M1 2021
-          </Text>
-          <Text style={{fontSize: 12, fontWeight: 'bold', color:'#f2994a', marginTop: 12}}>
-            Rp 21.000.000,-
-          </Text>
-          <Text style={{fontSize: 12, fontWeight: '300', marginTop: 12}}>
-            Bandar Lampung
-          </Text>
-          <View style={{
-            backgroundColor: '#6fcf97',
-            paddingVertical: 6,
-            borderRadius: 25,
-            marginTop: 20
-          }}>
-            <Text style={{
-              fontSize: 14,
-              fontWeight: '600',
-              color: 'white',
-              textAlign: 'center'
-            }}>
-              Beli
-              </Text>
-          </View>
-      </View>
-
+      <ScrollView>
+        <SampleComponent />
+        <StylingReactNativeComponent />
+      </ScrollView>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  text:  {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: '#adadad',
-    marginLeft: 20,
-    marginTop: 20
-  },
-});
-
-const Home = () => {
-  return <Text>Pratma</Text>;
-};
-
-const Photo = () => {
-return <Image source={{uri: 'https://placeimg.com/100/100/tech'}} style={{width: 100, height: 100}}/>
-}
-
-class BoxGreen extends Component {
-  render(){
-    return <Text>Ini Component dari Class</Text>
-  }
-}
-
-class Profile extends Component {
-  render(){
-    return(
-      <View>
-        <Image source={{uri: 'https://placeimg.com/100/100/arch'}} style={{width: 100, height: 100}} />
-        <Text>Ini Arch</Text>
-      </View>
-
-    );
-    
-  }
-}
 
 export default App;
---
+++ StylingReactNativeComponent.js
@@ -0,0 +1,80 @@
+/* eslint-disable no-unused-vars */
+import React, { Component } from 'react';
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
+import mackbook from './macbook.jpg';
+
+const StylingReactNativeComponent = () => {
+  return (
+    <View>
+      <Text style={styles.text}>Styling Component</Text>
+      <View
+        style={{
+          width: 100,
+          height: 100,
+          backgroundColor: '#00b894',
+          borderWidth: 2,
+          marginTop: 20,
+          marginLeft: 20,
+        }}
+      />
+
+      <View style={{ padding: 12, backgroundColor: '#f2f2f2', width: 212 }}>
+        <Image
+          source={mackbook}
+          style={{ width: 188, height: 107, borderRadius: 8 }}
+        />
+        <Text style={{ fontSize: 14, fontWeight: 'bold', marginTop: 16 }}>
+          New Macbook Pro M1 2021
+        </Text>
+        <Text
+          style={{
+            fontSize: 12,
+            fontWeight: 'bold',
+            color: '#f2994a',
+            marginTop: 12,
+          }}>
+          Rp 21.000.000,-
+        </Text>
+        <Text style={{ fontSize: 12, fontWeight: '300', marginTop: 12 }}>
+          Bandar Lampung
+        </Text>
+        <View
+          style={{
+            backgroundColor: '#6fcf97',
+            paddingVertical: 6,
+            borderRadius: 25,
+            marginTop: 20,
+          }}>
+          <Text
+            style={{
+              fontSize: 14,
+              fontWeight: '600',
+              color: 'white',
+              textAlign: 'center',
+            }}>
+            Beli
+          </Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#adadad',
+    marginLeft: 20,
+    marginTop: 20,
+  },
+});
+
+export default StylingReactNativeComponent;
