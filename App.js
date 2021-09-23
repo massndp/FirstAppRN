04ba9c39a7ff11938af40d08189f337f4990410f
massndp <bayu8989.88@gmail.com>
2021-09-23 11:05:29 +0700

    component dimensi, background, color, padding, margin & fontsize

--- App.js
+++ App.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
-import { Image, Text, TextInput, View} from 'react-native';
+import { Image, StyleSheet, Text, TextInput, View} from 'react-native';
+import mackbook from './macbook.jpg';
 
 const App = () => {
+  return <StylingReactNativeComponent />;
+      
+};
+
+
+const SampleComponent = () => {
   return (
     <View>
       <View style={{width: 100, height: 100, backgroundColor: '#00b894'}}/>
@@ -13,9 +20,66 @@ const App = () => {
       <TextInput style={{borderWidth: 1}} />
       <BoxGreen />
       <Profile />
-    </View>
-      );
+  </View>
+  );
 };
+
+
+const StylingReactNativeComponent = () => {
+  return(
+    <View>
+      <Text style={styles.text}>Styling Component</Text>
+      <View style={{
+          width: 100, 
+          height: 100,
+          backgroundColor: '#00b894', 
+          borderWidth: 2,
+          marginTop: 20,
+          marginLeft: 20
+         }} 
+      />
+
+      <View style={{padding: 12, backgroundColor: '#f2f2f2', width: 212}}>
+          <Image source={mackbook} style={{width: 188 , height: 107, borderRadius: 8}} />
+          <Text style={{fontSize: 14, fontWeight:'bold', marginTop: 16}}>
+           New Macbook Pro M1 2021
+          </Text>
+          <Text style={{fontSize: 12, fontWeight: 'bold', color:'#f2994a', marginTop: 12}}>
+            Rp 21.000.000,-
+          </Text>
+          <Text style={{fontSize: 12, fontWeight: '300', marginTop: 12}}>
+            Bandar Lampung
+          </Text>
+          <View style={{
+            backgroundColor: '#6fcf97',
+            paddingVertical: 6,
+            borderRadius: 25,
+            marginTop: 20
+          }}>
+            <Text style={{
+              fontSize: 14,
+              fontWeight: '600',
+              color: 'white',
+              textAlign: 'center'
+            }}>
+              Beli
+              </Text>
+          </View>
+      </View>
+
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  text:  {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#adadad',
+    marginLeft: 20,
+    marginTop: 20
+  },
+});
 
 const Home = () => {
   return <Text>Pratma</Text>;
